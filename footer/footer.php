2becaa527bd87a0f1300897e383d5b7ef67788a2
Sébastien <sbages.dev@gmail.com>
2021-11-28 23:47:25 +0100

    ref(accueil): videos, and new logos added

--- footer/footer.php
+++ footer/footer.php
@@ -1,6 +1,4 @@
-<footer>
-
-    <p class="footer no-margin">©<?= date('Y') ?> Les Bannis - <a href="../admin/index_admin.php" id="admin">Administration</a></p>
+<footer class="py-4">
+    <p class="footer no-margin">©<?= date('Y') ?> Les Bannis - Serveur français Conan Exiles - <a href="../admin/index_admin.php" id="admin">Administration</a></p>
     <p class="footer italic no-margin">Ce site n'utilise pas de "cookies" et ne nécessite pas de données personnelles</p>
-
 </footer>
--- view/accueil.php
+++ view/accueil.php
@@ -25,10 +25,11 @@ $port = $request_port->fetch(PDO::FETCH_ASSOC);
 <?php ob_start(); ?>
 
 <div id="title" class="flex">
-    <h1 id="main_title" class="center white no-margin hyborian">les bannis</h1>
-    <p class="subtitle center shadow-red white">Nouvelle version du site prochainement...</p>
-    <p class="subtitle center white no-margin">@IP : <?= $ip['value'] ?>:<?= $port['value'] ?></p>
-    <p class="size center white no-margin">Serveur français Conan Exiles</p>
+    <h1 id="main_title" class="center">
+        <img src="../img/banderole.gif" alt="" srcset="">
+    </h1>
+    <p class="subtitle center white no-margin">SERVEUR 1 : <?= $ip['value'] ?>:<?= $port['value'] ?></p>
+    <p class="subtitle center white no-margin">SERVEUR 2 : 213.246.45.68:7779</p>
 </div>
 
 <div id="triangle"></div>
@@ -37,11 +38,14 @@ $port = $request_port->fetch(PDO::FETCH_ASSOC);
 
     <aside id="aside_left" class="center">
         <h2 class="center shadow-red white">Rejoignez-nous sur discord</h2>
-        <iframe src="https://discordapp.com/widget?id=590837864510783489&theme=dark" class="mb-4"></iframe>
+        <a href="https://discord.gg/SMZJWyf" class="img-fluid link-discord" rel="noopener noreferrer nofollow">
+            <img src="../img/discord-logo-png-7617" alt="" srcset="">
+        </a>
         <br>
-        <a href="https://www.paypal.com/donate/?hosted_button_id=8TVEEZ9SQQV8A" target="_blank" rel="noopener noreferrer nofollow"><img id="paypal" src="../img/don.png" alt="faire un don avec paypal"></a>
-        <p id="help" class="white no-margin shadow-red mb-4">Aidez-nous à maintenir le serveur</p>
-        <br>
+        <h2 class="white no-margin shadow-red mt-4">Aidez-nous à maintenir le serveur</h2>
+        <a href="https://www.paypal.com/donate/?hosted_button_id=8TVEEZ9SQQV8A" target="_blank" rel="noopener noreferrer nofollow">
+            <img id="paypal" src="../img/don.png" alt="faire un don avec paypal" class="mb-4">
+        </a>
     </aside>        
 
     <section id="section_index" class="mb-5">
@@ -49,18 +53,20 @@ $port = $request_port->fetch(PDO::FETCH_ASSOC);
         <article class="white">
 
             <h2 class="center shadow-red">Bienvenue sur notre serveur</h2>
+            
+            <div class="flex youtube">
+                <iframe src="https://www.youtube.com/embed/28WnPvjCQb0" title="Présentation serveur Les Bannis" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
 
-            <p>Le contenu que nous proposons est avant tout qualitatif dans le respect de l'univers de Conan et le médiéval fantastique. Attaché au bien-être et retours des membres de la communauté, nous sommes ouverts aux propositions de chacun. Nous privilégions la création de mods dédiés pour notre serveur plutôt que les constructions in-game afin de préserver les performances des joueurs et la stabilité du serveur. Nous sommes des passionnés et avons de nombreux projets en cours pour apporter du contenu supplémentaire...</p>
+            <p class="m-2">Le contenu que nous proposons est avant tout qualitatif dans le respect de l'univers de Conan et le médiéval fantastique. Attaché au bien-être et retours des membres de la communauté, nous sommes ouverts aux propositions de chacun. Nous privilégions la création de mods dédiés pour notre serveur plutôt que les constructions in-game afin de préserver les performances des joueurs et la stabilité du serveur. Nous sommes des passionnés et avons de nombreux projets en cours pour apporter du contenu supplémentaire...</p>
 
-            <p>A ce titre, vous découvrirez Sobek, la cité principale de la terre des Bannis que nous avons entièrement modélisée de nos mains. Notre cité possède un marché avec de nombreux vendeurs et emplacements de vente réservés aux joueurs. Oserez-vous relever les défis de l'arène ? Nous sommes fier de vous proposer une arène moddée qui garantie une parfaite fluidité pendant les combats. Passez par la taverne afin de trouver du travail et partager une chope.</p>
+            <p class="m-2">Durant votre périple, vous découvrirez une multitude de quêtes uniques qui vous aideront à progresser. Elles sont régulièrement enrichies par une équipe de créateurs au top ! Vous trouverez également des quêtes et défis journalier pour agrémenter vos occupations à haut level.</p>
 
-            <p>Durant votre périple, vous découvrirez une multitude de quêtes uniques qui vous aideront à progresser. Elles sont régulièrement enrichies par une équipe de créateurs au top ! Vous trouverez également des quêtes et défis journalier pour agrémenter vos occupations à haut level.</p>
+            <div class="flex youtube">
+                <iframe src="https://www.youtube.com/embed/6OeSR0wugjw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
 
-            <p>En jeu, vous serez accompagnés par une équipe de modérateurs disponibles et à l'écoute. Notre communauté bienveillante sera également présente pour vous aider et vous accompagner dans votre progression. Nous n'avons pas vocation à faire du RP strict mais nous encourageons ce type d'échange et laissons libre choix aux joueurs. Ce que nous souhaitons avant tout est une bonne humeur, du respect, de l'humour et de la convivialité.</p>
-
-            <p>Enfin, selon quelques règles à respecter, vous pouvez vous faire plaisir et construire de belles demeures.</p>
-
-            <p>A bientôt !</p>
+            <p class="m-2">En jeu, vous serez accompagnés par une équipe de modérateurs disponibles et à l'écoute. Notre communauté bienveillante sera également présente pour vous aider et vous accompagner dans votre progression. Nous n'avons pas vocation à faire du RP strict mais nous encourageons ce type d'échange et laissons libre choix aux joueurs. Ce que nous souhaitons avant tout est une bonne humeur, du respect, de l'humour et de la convivialité.</p>
 
         </article>
 
